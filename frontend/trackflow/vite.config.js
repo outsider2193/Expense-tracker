aac6c889015d13f37a9844f7cf6144670d950cba
outsider2193 <c2kushalsingh@gmail.com>
2025-05-08 22:25:48 +0530

    downgrade react version and vite config changes

--- frontend/trackflow/vite.config.js
+++ frontend/trackflow/vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    mainFields: ['module', 'jsnext:main', 'jsnext', 'browser', 'main'],
+    alias: {
+      'react': 'react',
+      'react-dom': 'react-dom'
+    }
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
+});

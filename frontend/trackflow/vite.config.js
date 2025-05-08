7c1f20355635b6b2ebcbe9ede1b18310a8f0efc3
outsider2193 <c2kushalsingh@gmail.com>
2025-05-08 22:54:22 +0530

    vite config changes

--- frontend/trackflow/vite.config.js
+++ frontend/trackflow/vite.config.js
@@ -1,6 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    target: 'es2015',
+    minify: 'esbuild'
+  },
+  resolve: {
+    alias: {
+      // Only if you're using styled-components with MUI
+      '@mui/styled-engine': '@mui/styled-engine-sc'
+    }
+  }
+});

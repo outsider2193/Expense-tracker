99acbaa86e72307e0e32ef88759cef0b437f7a91
outsider2193 <c2kushalsingh@gmail.com>
2025-05-08 23:03:38 +0530

    reinstall dependencies

--- frontend/trackflow/vite.config.js
+++ frontend/trackflow/vite.config.js
@@ -3,14 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: 'es2015',
-    minify: 'esbuild'
-  },
-  resolve: {
-    alias: {
-      // Only if you're using styled-components with MUI
-      '@mui/styled-engine': '@mui/styled-engine-sc'
-    }
-  }
 });
